e26c654c080227300f4ccc33ef07bec61d9557c1
eajset <eajset@lucalza.com>
2023-08-31 20:51:48 -0600

    avance hasta mapper moviedb a movie

---
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,30 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infraestructure/models/movieDb_response.dart';
+import 'package:dio/dio.dart';
+
+import '../../domain/datasources/Movie_datasource.dart';
+
+class MovieDbDatasource extends MovieDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.movieDbKey,
+        'language': 'es-MX'
+      }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing', queryParameters: {
+      'page': page,
+    });
+
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDbResponse.results
+        .map((movieDb) => MovieMapper.movieDbToEntity(movieDb))
+        .toList();
+
+    return movies;
+  }
+}
---
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/models/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDbToEntity(MovieFromMovieDb movieDb) => Movie(
+      adult: movieDb.adult,
+      backdropPath: (movieDb.backdropPath) != ''
+          ? 'https://image.tmdb.org/t/p/w500/${movieDb.backdropPath}}'
+          : 'https://resourcesdev.blob.core.windows.net/resources-web/proyectos/poster-not-found.png',
+      genreIds: movieDb.genreIds.map((e) => e.toString()).toList(),
+      id: movieDb.id,
+      originalLanguage: movieDb.originalLanguage,
+      originalTitle: movieDb.originalTitle,
+      overview: movieDb.overview,
+      popularity: movieDb.popularity,
+      posterPath: (movieDb.posterPath) != ''
+          ? 'https://image.tmdb.org/t/p/w500/${movieDb.posterPath}}'
+          : 'no-poster',
+      releaseDate: movieDb.releaseDate,
+      title: movieDb.title,
+      video: movieDb.video,
+      voteAverage: movieDb.voteAverage,
+      voteCount: movieDb.voteCount);
+}
