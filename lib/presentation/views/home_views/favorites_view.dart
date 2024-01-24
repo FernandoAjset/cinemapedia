6d5271dd614234d02b217d3537b6c6ddd3246251
eajset <eajset@lucalza.com>
2024-01-24 11:36:42 -0600

    Added load of the favorite movies using masonry componente.

--- lib/presentation/views/home_views/favorites_view.dart
+++ lib/presentation/views/home_views/favorites_view.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+import '../../../domain/entities/movie.dart';
+import '../../providers/providers.dart';
+import '../../widgets/widgets.dart';
+
+class FavoritesView extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => FavoritesViewState();
+
   const FavoritesView({super.key});
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites view'),
-      ),
-      body: const Center(
-        child: Text('Favorites'),
-      ),
-    );
+    final Map<int, Movie>? movies = ref.watch(favoriteMoviesProvider);
+    if (movies == null) {
+      return const CircularProgressIndicator(
+        strokeWidth: 2,
+      );
+    }
+    // Convertir de mapa a lista de movie
+    final List<Movie> moviesList = movies.values.toList();
+
+    return Scaffold(body: MovieMasonry(movies: moviesList));
   }
 }
